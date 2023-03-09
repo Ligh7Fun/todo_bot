19ab684aa585a4252b122aa155f82679253c2788
Ligh7Fun <m5221710@gmail.com>
2023-03-09 23:01:04 +0600

    fixed

--- main.py
+++ main.py
@@ -5,22 +5,21 @@ import telebot
 bot = telebot.TeleBot(token)
 
 
-RANDOM_TASKS = [
-                'Написать Гвидо письмо',
+RANDOM_TASKS = ['Написать Гвидо письмо',
                 'Выучить Python',
                 'Записаться на курс в Нетологию',
-                'Посмотреть 4 сезон Рик и Морти',
-]
+                'Посмотреть 4 сезон Рик и Морти'
+                ]
 
 todos = dict()
 
 
 HELP = '''
 Список доступных команд:
-* show  - напечать все задачи на заданную дату
-* todo - добавить задачу
-* random - добавить на сегодня случайную задачу
-* help - Напечатать help
+* show [date]- напечать все задачи на заданную дату
+* add [date task] - добавить задачу
+* /random - добавить на сегодня случайную задачу
+* /help - Напечатать help
 '''
 
 
@@ -41,29 +40,34 @@ def help(message):
 def random(message):
     task = choice(RANDOM_TASKS)
     add_todo('сегодня', task)
-    bot.send_message(message.chat.id, 
-                     f'Задача {task} добавлена на сегодня')
+    bot.send_message(message.chat.id, f'Задача {task} добавлена на сегодня')
 
 
 @bot.message_handler(commands=['add'])
 def add(message):
     _, date, tail = message.text.split(maxsplit=2)
     task = ' '.join([tail])
+    # TODO: 1
+    if len(task) < 3:
+        bot.send_message(message.chat.id,
+                         'Задачи должны быть больше 3х символов')
     add_todo(date, task)
-    bot.send_message(message.chat.id, 
+    bot.send_message(message.chat.id,
                      f'Задача {task} добавлена на дату {date}')
 
 
 @bot.message_handler(commands=['show'])
 def print_(message):
-    date = message.text.split()[1].lower()
-    if date in todos:
-        tasks = ''
-        for task in todos[date]:
-            tasks += f'[ ] {task}\n'
-    else:
-        tasks = 'Такой даты нет'
-    bot.send_message(message.chat.id, tasks)
+    # TODO: 2
+    dates = message.text.split(maxsplit=1)[1].lower().split()
+    response = ''
+    for date in dates:
+        tasks = todos.get(date)
+        response += f'{date}: \n'
+        for task in tasks:
+            response += f'[ ] {task}\n'
+        response += '\n'
+    bot.send_message(message.chat.id, response)
 
 
 bot.polling(none_stop=True)
